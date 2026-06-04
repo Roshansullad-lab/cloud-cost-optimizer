94da0cdc2134b702f3384bac6304fce3eebf0f5a
sullad855 <sullad855@gmail.com>
2026-06-05 00:09:45 +0530

    updated paths to deploy on cloud docker container services like render

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-DATABASE_URL = "sqlite:///./finops_optimizer.db"
+#DATABASE_URL = "sqlite:///./finops_optimizer.db"
+DATABASE_URL = "sqlite:////tmp/finops_optimizer.db"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
